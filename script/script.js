91d1c7af7c261010519d36ca67b176683cc214eb
Nikita <nikita119@protonmail.com>
2023-10-22 19:30:01 +0300

    localStorage && backgroudApi && refresh background

--- script/script.js
+++ script/script.js
@@ -3,45 +3,40 @@ document.addEventListener('DOMContentLoaded', () =>{
 // Date
     const time = document.querySelector('.wrapper__date-time');
     const date = document.querySelector('.wrapper__date-date');
-    const greetings = document.querySelector('.wrapper__date-greetings')
+    const greetings = document.querySelector('.wrapper__date-greetings');
     const hourSpan = document.querySelectorAll('.time_hour');
     const minSpan = document.querySelectorAll('.time_min');
     const secSpan = document.querySelectorAll('.time_sec');
-    const checkerAMPM = (new Date()).toLocaleString('en-US', { hour: 'numeric', hour12: true })[2];
     const spanPM = document.createElement('span');
     const spanAM = document.createElement('span');
     spanPM.innerText = 'PM';
     spanAM.innerText = 'AM';
-    if (checkerAMPM === 'A') {
+    if ((new Date()).toTimeString().substring(0,2) >= 1 && (new Date()).toTimeString().substring(0,2) <= 12) {
         time.append(spanAM);
+    } else if ((new Date()).toTimeString().substring(0,2) >= 13 || (new Date()).toTimeString().substring(0,2) === '00') {
+        time.append(spanPM);
     }
-    time.append(spanPM)
+
     setInterval(()=> {
         const date = new Date();
-        const currentHours = date.toTimeString().substring(0,2)
-        const currentMin = date.toTimeString().substring(3,5)
-        const currentSec = date.toTimeString().substring(6,8)
-        if (checkerAMPM === 'A') {
+        const currentHours = date.toTimeString().substring(0, 2);
+        const currentMin = date.toTimeString().substring(3, 5);
+        const currentSec = date.toTimeString().substring(6, 8);
+        if (currentHours === '00' || (currentHours >= 1 && currentHours <= 12)) {
             hourSpan[0].innerHTML = currentHours[0];
             hourSpan[1].innerHTML = currentHours[1];
-            minSpan[0].innerHTML = currentMin[0];
-            minSpan[1].innerHTML = currentMin[1];
-            secSpan[0].innerHTML = currentSec[0];
-            secSpan[1].innerHTML = currentSec[1];
-        } else if (checkerAMPM === 'P') {
-            minSpan[0].innerHTML = currentMin[0];
-            minSpan[1].innerHTML = currentMin[1];
-            secSpan[0].innerHTML = currentSec[0];
-            secSpan[1].innerHTML = currentSec[1];
-            if(currentHours - 12 < 10) {
-                hourSpan[0].innerHTML = '0';
-                hourSpan[1].innerHTML = currentHours - 12;
-            } else if (currentHours >= 10) {
-                hourSpan[0].innerHTML = currentHours[0];
-                hourSpan[1].innerHTML = currentHours[1];
-            }
-
+        } else if (currentHours >= 13 && currentHours <= 21) {
+            hourSpan[0].innerHTML = '0';
+            hourSpan[1].innerHTML = currentHours - 12;
+        } else if (currentHours >= 23) {
+            hourSpan[0].innerHTML = '1';
+            hourSpan[1].innerHTML = (currentHours - 12)[1];
         }
+        minSpan[0].innerHTML = currentMin[0];
+        minSpan[1].innerHTML = currentMin[1];
+        secSpan[0].innerHTML = currentSec[0];
+        secSpan[1].innerHTML = currentSec[1];
+
     },1000);
     const dateNow = new Date();
     let currentWeekday = dateNow.getDay();
@@ -144,22 +139,44 @@ document.addEventListener('DOMContentLoaded', () =>{
     }
     date.innerText = `${currentWeekday}, ${currentDay} of ${currentMonth}`;
 
-    // Refresh
-
-    const refreshBtn = document.querySelector('.refresh');
-
-    refreshBtn.addEventListener('click', () => {
-        refreshBtn.classList.add('rotateClass');
-        setTimeout(() => {
-            refreshBtn.classList.remove('rotateClass');
-        }, 800)
-    })
 
     // Localstorage
 
     const spans = document.querySelectorAll('.wrapper__date-span');
     const nameTitle = document.querySelector('[data-name]');
     const focusTitle = document.querySelector('[data-focus]');
+
+    spans.forEach((item,i) => {
+        item.addEventListener('keydown', (e) => {
+            if (e.target.innerText.length >= 26 && i === 0) {
+                if (e.keyCode === 8 || e.keyCode === 46 || e.keyCode === 37 || e.keyCode === 39) {
+                    return;
+                }
+                e.preventDefault();
+            } else if (i === 1 && e.target.innerText.length >= 39) {
+                if (e.keyCode === 8 || e.keyCode === 46 || e.keyCode === 37 || e.keyCode === 39) {
+                    return;
+                }
+                e.preventDefault();
+            }
+        });
+
+        item.addEventListener('focus', (e)=> {
+            if (e.target.textContent === '[ENTER NAME]' || e.target.textContent === '[ENTER FOCUS]') {
+                e.target.textContent = null;
+            }
+        })
+        item.addEventListener('blur', (e) => {
+            if (e.target.textContent === '' && i === 0) {
+                e.target.textContent = '[ENTER NAME]';
+            }
+            if (e.target.textContent === '' && i === 1) {
+                e.target.textContent = '[ENTER FOCUS]';
+            }
+        })
+    });
+
+
 
     function init(localKey,title,name,backup) {
         getDataFromLocalStorage(localKey, title,name);
@@ -198,37 +215,64 @@ document.addEventListener('DOMContentLoaded', () =>{
 
 
     window.addEventListener('load', init.bind(null,'nameTitle',nameTitle,'NAME', 'backupTitle'));
+    window.addEventListener('load', init.bind(null,'focusTitle',focusTitle,'FOCUS', 'backupFocus'));
     nameTitle.addEventListener('keydown', e => changeTitle(e,'nameTitle', 'backupTitle'));
     nameTitle.addEventListener('blur', e => saveToLocalStorageAfterBlur(e, 'nameTitle', 'backupTitle'));
 
-    window.addEventListener('load', init.bind(null,'focusTitle',focusTitle,'FOCUS', 'backupFocus'));
+
     focusTitle.addEventListener('keydown', e => changeTitle(e,'focusTitle', 'backupFocus'));
     focusTitle.addEventListener('blur', e => saveToLocalStorageAfterBlur(e, 'focusTitle', 'backupFocus'));
-    spans.forEach((item,i) => {
-        item.addEventListener('keydown', (e) => {
-            if (e.target.innerText.length >= 26 && i === 0) {
-                if (e.keyCode === 8 || e.keyCode === 46 || e.keyCode === 37 || e.keyCode === 39) {
-                    return;
-                }
-                e.preventDefault();
-            } else if (i === 1 && e.target.innerText.length >= 39) {
-                if (e.keyCode === 8 || e.keyCode === 46 || e.keyCode === 37 || e.keyCode === 39) {
-                    return;
-                }
-                e.preventDefault();
-            }
-        });
 
-        // item.addEventListener('focus', (e)=> {
-        //     e.target.textContent = '';
-        // })
-        // item.addEventListener('blur', (e) => {
-        //     // console.log(backupTitle)
-        //     e.target.textContent = backupTitle;
-        // })
+// Api background image
+
+     function backgroundImage(api, initFlag) {
+        fetch(api)
+            .then(response => {
+                const img = document.createElement('img');
+                img.src = response.url;
+                img.addEventListener('load', () => {
+                    if (initFlag) {
+                        document.body.style.backgroundImage = `url("${img.src}")`;
+                        document.body.style.backgroundRepeat = 'no-repeat';
+                    } else if (!initFlag) {
+                        const backgroundFrame =  document.querySelector('.background');
+                        document.body.style.backgroundColor = 'rgba(0,0,0,.1)';
+                        document.body.style.backgroundImage = `url("${img.src}")`;
+                        document.body.style.backgroundRepeat = 'no-repeat';
+                        backgroundFrame.classList.add('opacityBG');
+                        setTimeout(()=> {
+                            backgroundFrame.classList.remove('opacityBG');
+                        },400)
+                    }
+
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    }
+        backgroundImage('https://random.imagecdn.app/1920/1080', true);
+
+
+    // Refresh
+
+    const refreshBtn = document.querySelector('.refresh');
+
+    refreshBtn.addEventListener('click', () => {
+        refreshBtn.classList.add('rotateClass');
+        setTimeout(() => {
+            refreshBtn.classList.remove('rotateClass');
+        }, 800)
     });
 
+    refreshBtn.addEventListener('click', () => {
+        backgroundImage('https://random.imagecdn.app/1920/1080', false);
+    })
 })
+
+
+
 
 
 
