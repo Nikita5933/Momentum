b5e5f4581021bfacb5190c6dfadf3f85e258e76b
Nikita <nikita119@protonmail.com>
2023-10-28 19:06:52 +0300

    weather completed

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 "use strict";
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
 // Date
     const time = document.querySelector('.wrapper__date-time');
     const date = document.querySelector('.wrapper__date-date');
@@ -11,13 +11,12 @@ document.addEventListener('DOMContentLoaded', () =>{
     const spanAM = document.createElement('span');
     spanPM.innerText = 'PM';
     spanAM.innerText = 'AM';
-    if ((new Date()).toTimeString().substring(0,2) >= 1 && (new Date()).toTimeString().substring(0,2) <= 12) {
+    if ((new Date()).toTimeString().substring(0, 2) >= 1 && (new Date()).toTimeString().substring(0, 2) <= 12) {
         time.append(spanAM);
-    } else if ((new Date()).toTimeString().substring(0,2) >= 13 || (new Date()).toTimeString().substring(0,2) === '00') {
+    } else if ((new Date()).toTimeString().substring(0, 2) >= 13 || (new Date()).toTimeString().substring(0, 2) === '00') {
         time.append(spanPM);
     }
-
-    setInterval(()=> {
+    setInterval(() => {
         const date = new Date();
         const currentHours = date.toTimeString().substring(0, 2);
         const currentMin = date.toTimeString().substring(3, 5);
@@ -30,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () =>{
             hourSpan[1].innerHTML = currentHours - 12;
         } else if (currentHours >= 23) {
             hourSpan[0].innerHTML = '1';
-            hourSpan[1].innerHTML = (currentHours-12).toString()[1];
+            hourSpan[1].innerHTML = (currentHours - 12).toString()[1];
         } else if (currentHours == '00') {
             hourSpan[0].innerHTML = '1';
             hourSpan[1].innerHTML = (+currentHours + 12).toString()[1];
@@ -40,12 +39,12 @@ document.addEventListener('DOMContentLoaded', () =>{
         secSpan[0].innerHTML = currentSec[0];
         secSpan[1].innerHTML = currentSec[1];
 
-    },1000);
+    }, 1000);
     const dateNow = new Date();
     let currentWeekday = dateNow.getDay();
     let currentDay = dateNow.getDate();
     let currentMonth = dateNow.getMonth();
-    let currentTimeGreetings = +dateNow.toTimeString().substring(0,2);
+    let currentTimeGreetings = +dateNow.toTimeString().substring(0, 2);
 
     if (currentTimeGreetings >= 0 && currentTimeGreetings <= 5) {
         greetings.innerText += ' Night,';
@@ -149,7 +148,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     const nameTitle = document.querySelector('[data-name]');
     const focusTitle = document.querySelector('[data-focus]');
 
-    spans.forEach((item,i) => {
+    spans.forEach((item, i) => {
         item.addEventListener('keydown', (e) => {
             if (e.target.innerText.length >= 26 && i === 0) {
                 if (e.keyCode === 8 || e.keyCode === 46 || e.keyCode === 37 || e.keyCode === 39) {
@@ -164,7 +163,7 @@ document.addEventListener('DOMContentLoaded', () =>{
             }
         });
 
-        item.addEventListener('focus', (e)=> {
+        item.addEventListener('focus', (e) => {
             if (e.target.textContent === '[ENTER NAME]' || e.target.textContent === '[ENTER FOCUS]') {
                 e.target.textContent = null;
             }
@@ -180,16 +179,15 @@ document.addEventListener('DOMContentLoaded', () =>{
     });
 
 
-
-    function init(localKey,title,name,backup) {
-        getDataFromLocalStorage(localKey, title,name);
+    function init(localKey, title, name, backup) {
+        getDataFromLocalStorage(localKey, title, name);
         saveToLocalStorage(backup, title.textContent);
     }
 
-    function changeTitle(e,nameOfTitle, backup) {
+    function changeTitle(e, nameOfTitle, backup) {
         if (e.keyCode === 13) {
             saveToLocalStorage(nameOfTitle, e.target.textContent);
-            saveToLocalStorage(backup,e.target.textContent);
+            saveToLocalStorage(backup, e.target.textContent);
             e.target.blur();
         }
         if (e.keyCode === 27) {
@@ -202,7 +200,7 @@ document.addEventListener('DOMContentLoaded', () =>{
         localStorage.setItem(name, value);
     }
 
-    function getDataFromLocalStorage(localKey,title,name) {
+    function getDataFromLocalStorage(localKey, title, name) {
         let data = localStorage.getItem(localKey);
         if (data) {
             title.textContent = data;
@@ -210,25 +208,26 @@ document.addEventListener('DOMContentLoaded', () =>{
         }
         title.textContent = `[ENTER ${name}]`;
     }
-    function saveToLocalStorageAfterBlur(e, nameTitle,backup) {
+
+    function saveToLocalStorageAfterBlur(e, nameTitle, backup) {
         saveToLocalStorage(backup, e.target.textContent)
         saveToLocalStorage(nameTitle, e.target.textContent)
     }
 
 
-
-    window.addEventListener('load', init.bind(null,'nameTitle',nameTitle,'NAME', 'backupTitle'));
-    window.addEventListener('load', init.bind(null,'focusTitle',focusTitle,'FOCUS', 'backupFocus'));
-    nameTitle.addEventListener('keydown', e => changeTitle(e,'nameTitle', 'backupTitle'));
+    window.addEventListener('load', init.bind(null, 'nameTitle', nameTitle, 'NAME', 'backupTitle'));
+    window.addEventListener('load', init.bind(null, 'focusTitle', focusTitle, 'FOCUS', 'backupFocus'));
+    nameTitle.addEventListener('keydown', e => changeTitle(e, 'nameTitle', 'backupTitle'));
     nameTitle.addEventListener('blur', e => saveToLocalStorageAfterBlur(e, 'nameTitle', 'backupTitle'));
 
 
-    focusTitle.addEventListener('keydown', e => changeTitle(e,'focusTitle', 'backupFocus'));
+    focusTitle.addEventListener('keydown', e => changeTitle(e, 'focusTitle', 'backupFocus'));
     focusTitle.addEventListener('blur', e => saveToLocalStorageAfterBlur(e, 'focusTitle', 'backupFocus'));
 
-// Api background image
+// API background image
 
-     function backgroundImage(api, initFlag) {
+
+    function backgroundImage(api, initFlag) {
         fetch(api)
             .then(response => {
                 const img = document.createElement('img');
@@ -238,14 +237,14 @@ document.addEventListener('DOMContentLoaded', () =>{
                         document.body.style.backgroundImage = `url("${img.src}")`;
                         document.body.style.backgroundRepeat = 'no-repeat';
                     } else if (!initFlag) {
-                        const backgroundFrame =  document.querySelector('.background');
+                        const backgroundFrame = document.querySelector('.background');
                         document.body.style.backgroundColor = 'rgba(0,0,0,.1)';
                         document.body.style.backgroundImage = `url("${img.src}")`;
                         document.body.style.backgroundRepeat = 'no-repeat';
                         backgroundFrame.classList.add('opacityBG');
-                        setTimeout(()=> {
+                        setTimeout(() => {
                             backgroundFrame.classList.remove('opacityBG');
-                        },400)
+                        }, 400)
                     }
 
                 })
@@ -255,8 +254,8 @@ document.addEventListener('DOMContentLoaded', () =>{
             })
 
     }
-        backgroundImage('https://random.imagecdn.app/1920/1080', true);
 
+    backgroundImage('https://random.imagecdn.app/1920/1080', true);
 
     // Refresh
 
@@ -272,10 +271,212 @@ document.addEventListener('DOMContentLoaded', () =>{
     refreshBtn.addEventListener('click', () => {
         backgroundImage('https://random.imagecdn.app/1920/1080', false);
     })
+
+    // Weather API
+    const CURRENT_API_WEATHER_KEY = '161d11578995b0dd48447acb004b6a41';
+    const wrapper = document.querySelector('.wrapper__weather-block');
+    const city = document.querySelector('.city');
+
+    let status;
+
+    function getWeather(value) {
+        const dataObj= {};
+        function changeIcon(value) {
+            const div = document.createElement('div');
+            div.classList.add('wrapper__weather-pict');
+
+            switch (value.id) {
+                case 200:
+                case 201:
+                case 202:
+                case 230:
+                case 231:
+                case 232:
+                    div.style.backgroundPosition = "-70px 617px";
+                    div.style.backgroundSize = "865%";
+                    break;
+                case 210:
+                case 211:
+                case 212:
+                case 221:
+                    div.style.backgroundPosition = "-275px 617px";
+                    div.style.backgroundSize = "865%";
+                    break;
+                case 300:
+                case 310:
+                    div.style.backgroundPosition = "-70px 420px";
+                    div.style.backgroundSize = "865%";
+                    break;
+                case 301:
+                case 311:
+                    div.style.backgroundPosition = "-407px 224px";
+                    div.style.backgroundSize = "725%";
+                    break;
+                case 302:
+                case 312:
+                case 313:
+                case 314:
+                case 321:
+                    div.style.backgroundPosition = "-231px 224px";
+                    div.style.backgroundSize = "725%";
+                    break;
+                case 500:
+                case 501:
+                case 520:
+                case 531:
+                    div.style.backgroundPosition = "-580px 547px";
+                    div.style.backgroundSize = "725%";
+                    break;
+                case 511:
+                case 613:
+                case 615:
+                case 616:
+                    div.style.backgroundPosition = "-484px 422px";
+                    div.style.backgroundSize = "865%";
+                    break;
+                case 502:
+                case 503:
+                case 504:
+                case 522:
+                    div.style.backgroundPosition = "-690px 423px";
+                    div.style.backgroundSize = "865%";
+                    break;
+                case 600:
+                case 611:
+                case 612:
+                    div.style.backgroundPosition = "207px 439px";
+                    div.style.backgroundSize = "865%";
+                    break;
+                case 601:
+                case 602:
+                case 620:
+                case 621:
+                case 622:
+                    div.style.backgroundPosition = "-70px 246px";
+                    div.style.backgroundSize = "865%";
+                    break;
+                case 701:
+                case 711:
+                case 721:
+                case 731:
+                case 741:
+                case 751:
+                case 762:
+                    div.style.backgroundPosition = "408px 246px";
+                    div.style.backgroundSize = "865%";
+                    break;
+                case 771:
+                case 781:
+                    div.style.backgroundPosition = "219px 246px";
+                    div.style.backgroundSize = "865%";
+                    break;
+                case 800:
+                    div.style.backgroundPosition = "390px 645px";
+                    div.style.backgroundSize = "865%";
+                    break;
+                case 801:
+                    div.style.backgroundPosition = "-231px 381px";
+                    div.style.backgroundSize = "725%";
+                    break;
+                case 802:
+                    div.style.backgroundPosition = "344px 381px";
+                    div.style.backgroundSize = "725%";
+                    break;
+                case 803:
+                    div.style.backgroundPosition = "-481px 636px";
+                    div.style.backgroundSize = "865%";
+                    break;
+                case 804:
+                    div.style.backgroundPosition = "508px 228px";
+                    div.style.backgroundSize = "725%";
+                    break;
+
+            }
+           wrapper.append(div)
+        }
+        function renderHTML(obj) {
+            const div = `
+            <div class="wrapper__weather-info">
+            <div class="wrapper__weather-temp"><span>${obj.temp}</span> C°</div>
+            <div class="wrapper__weather-description">Weather:<span> ${obj.desc}</span></div>
+            <div class="wrapper__weather-humidity">Humidity:<span> ${obj.humidity} %</span></div>
+            <div class="wrapper__weather-wind">Wind speed: <span>${obj.wind} m/s</span></div>
+            </div>
+            `;
+
+            wrapper.innerHTML = '';
+            changeIcon(obj)
+            wrapper.insertAdjacentHTML('beforeend', div)
+        }
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=3&appid=${CURRENT_API_WEATHER_KEY}`)
+            .then(response => {
+                if (response.ok) return response.json();
+                status = response.status;
+            })
+            .then(([geo]) => {
+                fetch(`https://api.openweathermap.org/data/2.5/weather?lang=en&units=metric&lat=${geo.lat}&lon=${geo.lon}&appid=${CURRENT_API_WEATHER_KEY}`)
+                    .then(data => {
+                        if (data.ok) return data.json();
+                    })
+                    .then(({main, weather, wind}) => {
+                        dataObj.temp = Math.round(main.temp);
+                        dataObj.humidity = main.humidity;
+                        dataObj.desc = weather[0].description;
+                        dataObj.id = weather[0].id;
+                        dataObj.wind = wind.speed;
+                        return dataObj
+                    })
+                    .then((obj) => {
+                        renderHTML(obj);
+                    })
+                    .catch(err => {
+                        const error = `<div class="wrapper__weather-error">Server error! Try later!</div>`;
+                        wrapper.innerText = '';
+                        wrapper.insertAdjacentHTML('beforeend', error);
+                        console.log(err);
+                    })
+            })
+            .catch(err => {
+                let error = `<div class="wrapper__weather-error">City data search error, please try to enter the city correctly!</div>`;
+                if (status) {
+                    error = `<div class="wrapper__weather-error">Server error! Try later!</div>`;
+                }
+                wrapper.innerText = '';
+                wrapper.insertAdjacentHTML('beforeend', error);
+                console.log(err);
+            })
+
+    }
+    function initWeather() {
+        if (localStorage.getItem('city')) {
+            getWeather(localStorage.getItem('city'));
+            city.innerText = localStorage.getItem('city')
+        }
+    }
+    city.addEventListener('keydown', e => {
+        if (e.keyCode === 13 || e.keyCode === 27) {
+            city.blur();
+            const value = city.innerText.trim();
+            saveToLocalStorage('city',value);
+            getWeather(value);
+        }
+    })
+    city.addEventListener('focus', (e)=> {
+        if (e.target.innerText === '[ENTER CITY]' || wrapper.children[0].classList.value === 'wrapper__weather-error') {
+           e.target.innerText = '';
+           saveToLocalStorage('city', '')
+        }
+    })
+    city.addEventListener('blur', (e) => {
+        if (e.target.innerText === '') {
+            e.target.innerText = '[ENTER CITY]';
+            saveToLocalStorage('city', '')
+            wrapper.innerText = '';
+            return;
+        }
+        const value = city.innerText.trim();
+        saveToLocalStorage('city',value);
+        getWeather(value);
+    })
+    initWeather();
 })
-
-
-
-
-
-
